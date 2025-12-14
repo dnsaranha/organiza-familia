08cd1589ab7254cfdaf4127ecb917dbf6c528811
Name <dns.aranha@gmail.com>
2025-12-14 21:35:51 +0000

    lista e cores

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,9 @@ import { componentTagger } from "lovable-tagger";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
-    host: "0.0.0.0",
+    // @ts-ignore
+    allowedHosts: true,
+    host: '0.0.0.0',
     port: 8080,
     hmr: {
       clientPort: 443,
